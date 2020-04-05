43c5fb2f156852b869274b485a2596b291bbea0c
pandaterminator <pandaterminator01@gmail.com>
2020-04-05 17:42:58 +0900

    change title

--- src/App.js
+++ src/App.js
@@ -6,21 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          test travis build
-          mission sucess
-          once again
+          Tool for investigate and so on...
+          Back soon
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
